5b31ba279e56fdb7d171e83576e8ad7e02f8424a
LinconlBR <linconl1997@gmail.com>
2026-05-19 13:59:47 +0200

    Adiciona componente Input com estilos e estados básicos
    Adiciona Input reutilizável com suporte a placeholder, required, type, e estados (hover, focus, disabled, erro, sucesso). Alinha estilos ao tema global definido em App.css.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import Botao from '../components/Botao/Botao';
+import Input from '../components/Input/Input';
+
 
 export default function Home() {
   function handleCliqueBotao() {  
     alert("Botão clicado!");
   }
-  
+  function handleInputSobrenome() {
+        console.log("Sobrenome mudou");
+    }
   return (
     <div className="home">
       <h1>Welcome to Home</h1>
       <p>This is the home page of your React application.</p>
-
+      
+      <h2>Buttons</h2>
       <div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
         <Botao>Normal</Botao>
         <Botao contorno >Outline</Botao>
         <Botao className="extra-class" onClick={handleCliqueBotao}>Customizable</Botao>
+    </div>
+
+    <h2>Inputs</h2>
+    <div style={{display: "flex", alignItems: "center", gap: "20px", flexWrap: "wrap"}}>
+        <Input placeholder="Nome" />
+        <Input placeholder="Sobrenome" onInput={handleInputSobrenome} />
+        <Input placeholder="Email" type="email" required />
     </div>
     </div>
   );
