d642ed07d4e4f23e77750c4e6942fb9f2519c4a1
LinconlBR <linconl1997@gmail.com>
2026-05-19 15:45:21 +0200

    Roteamento básico e páginas estáticas
    -Início (Home)
    -Sobre (About)
    -Produtos (Products)
    -Carrinho (Cart)
    
    As páginas Início e Sobre são páginas estáticas, enquanto as páginas Produtos e Carrinho são dinâmicas

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+ 
+  return (
+    <div className="about">
+      <h1>Welcome to About</h1>
+      <p>This is the about page of your React application.</p>
+      
+    </div>
+  );
+}
---
+++ src/pages/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import Botao from '../components/Botao/Botao';
+//import Input from '../components/Input/Input';
+
+
+export default function Products() {
+ 
+  return (
+    <div className="products">
+      <h1>Welcome to Products</h1>
+      <p>This is the products page of your React application.</p>
+      
+    </div>
+  );
+}
